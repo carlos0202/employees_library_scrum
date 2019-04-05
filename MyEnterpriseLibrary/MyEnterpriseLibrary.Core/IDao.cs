239b73889c9f5a9bce4d1e1381bee36cc6192442
Carlos González <carlos0202@users.noreply.github.com>
2019-04-05 18:04:09 -0400

    Ssync

--- MyEnterpriseLibrary/MyEnterpriseLibrary.Core/IDao.cs
+++ MyEnterpriseLibrary/MyEnterpriseLibrary.Core/IDao.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MyEnterpriseLibrary.Core
 {
-    public interface IDao<Book>
+    public interface IDao
     {
         bool Add(Book t);
         bool Update(Book t, string id);
