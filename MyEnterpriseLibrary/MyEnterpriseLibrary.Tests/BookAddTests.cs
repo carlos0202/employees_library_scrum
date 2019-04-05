2d3a9bf14afcc4872606dfd92c631d0081aa4f52
Carlos González <carlos0202@users.noreply.github.com>
2019-04-05 16:22:07 -0400

    Fisrt blood
    xD

---
+++ MyEnterpriseLibrary/MyEnterpriseLibrary.Tests/BookAddTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEnterpriseLibrary.Tests
+{
+    [TestFixture]
+    public class BookAddTests
+    {
+        /*
+        * 1. Add Book with ISBN, Title and author.
+        */
+        [Test]
+        public void Add_Book_With_All_Info()
+        {
+            // Arrange
+            var book = new Book(iSBN: "A123456Z", )
+        }
+    }
+}
