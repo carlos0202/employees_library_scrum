322dd22a6aa41dcb40f77e42106476c490fcbd6b
juanchinovas2 <49323495+juanchinovas2@users.noreply.github.com>
2019-04-05 17:07:49 -0400

    Sync

---
+++ MyEnterpriseLibrary/MyEnterpriseLibrary.Core/BookLender.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyEnterpriseLibrary.Core
+{
+    public class BookLender
+    {
+        public BookLender()
+        {
+        }
+
+        public bool lend(string bookId, int employeeId)
+        {
+            return true;
+        }
+    }
+}
---
+++ MyEnterpriseLibrary/MyEnterpriseLibrary.Tests/BookLendTest.cs
@@ -0,0 +1,27 @@
+﻿using MyEnterpriseLibrary.Core;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEnterpriseLibrary.Tests
+{
+    [TestFixture]
+    class BookLendTest
+    {
+
+        [Test]
+        public void lend_book_to_employee() {
+            //Arrange
+            string bookId = "E0";
+            int employeeId = 1;
+            BookLender bookLender = new BookLender();
+            //Act
+            bool status = bookLender.lend(bookId, employeeId);
+            //Assert
+            Assert.That(status, Is.EqualTo(true));
+        }
+    }
+}
