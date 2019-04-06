0db954d86f3f86f7a960db4d47db15af3b44f334
Carlos González <carlos0202@users.noreply.github.com>
2019-04-06 10:05:20 -0400

    add book added

--- MyEnterpriseLibrary/MyEnterpriseLibrary.Core/BookDaoPROD.cs
+++ MyEnterpriseLibrary/MyEnterpriseLibrary.Core/BookDaoPROD.cs
@@ -10,27 +10,38 @@ namespace MyEnterpriseLibrary.Core
 {
     public class BookDaoPROD : IDao
     {
-        private List<Book> _books;
-        private string _dbUrl = "/DB.json";
+        private Db db;
+        private string _dbUrl = "DB.json";
         public BookDaoPROD()
         {
-            using(StreamReader r = new StreamReader(Directory.GetCurrentDirectory() + _dbUrl))
-            {
-                string json = r.ReadToEnd();
-                try
-                {
-                    dynamic _db = JsonConvert.DeserializeObject(json);
-                    _books = JsonConvert.DeserializeObject<List<Book>>(_db.books.ToString());
-                }
-                catch {
-                }
-                
-            }
+            GetDb();
         }
 
         public bool Add(Book t)
         {
-            throw new NotImplementedException();
+            if (db.Books.Any(b => b.ISBN == t.ISBN))
+            {
+                throw new ArgumentException("A book with this ISBN already exists.");
+            }
+
+            if (t.ISBN == null)
+            {
+                throw new ArgumentNullException(null, "ISBN cannot be null.");
+            }
+
+            if (t.Title == null)
+            {
+                throw new ArgumentNullException(null, "Title cannot be null.");
+            }
+
+            if (t.Authors == null)
+            {
+                throw new ArgumentNullException(null, "Author cannot be null.");
+            }
+
+            db.Books.Add(t);
+
+            return true;
         }
 
         public List<Book> GetAll()
@@ -43,5 +54,37 @@ namespace MyEnterpriseLibrary.Core
             throw new NotImplementedException();
         }
 
+
+        protected void GetDb()
+        {
+            using (StreamReader r = new StreamReader(Directory.GetCurrentDirectory() + "/" +  _dbUrl))
+            {
+                string json = r.ReadToEnd();
+                try
+                {
+                    dynamic _db = JsonConvert.DeserializeObject<Db>(json);
+                }
+                catch
+                {
+                }
+
+            }
+        }
+
+        protected void FlushDb()
+        {
+            // serialize JSON directly to a file again
+            using (StreamWriter file = File.CreateText(_dbUrl))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, db);
+            }
+        }
+    }
+
+    internal class Db
+    {
+        public List<Book> Books { get; set; }
+        public object Employees { get; set; }
     }
 }
